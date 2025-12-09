cb64e51af49c26f4558eb8fa51253f21ccacfeb3
TEN3T <esther78944@gmail.com>
2025-12-09 10:06:21 +0900

    Update: Next.js update
    +15.4.7 -> 16.0.8

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  // 개발 모드에서 오류 오버레이 비활성화
-  devIndicators: {
-    position: 'bottom-right',
-  },
   // React의 오류 오버레이 비활성화
   reactStrictMode: false,
 };
